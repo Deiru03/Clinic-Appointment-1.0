843d01e1078ad7258c60307e5f78e1f2a893c8b8
Dale1003 <deiru03@gmail.com>
2025-01-25 13:16:17 +0800

    End User hidden History Page

--- sidebar.php
+++ sidebar.php
@@ -288,12 +288,13 @@
                   <p>On Progress</p>
                 </a>
               </li>
+              <!-- Temporary Hidden -->
               <li class="nav-item">
-                <a href="./index.php?page=my_accomplished_appointment_list" class="nav-link nav-my_accomplished_appointment_list tree-item">
+                <!-- <a href="./index.php?page=my_accomplished_appointment_list" class="nav-link nav-my_accomplished_appointment_list tree-item">
                   <i class="fas fa-angle-right nav-icon"></i>
                   <p>History of Treatment</p>
                 </a>
-              </li>
+              </li> -->
             </ul>
           </li>
           <li class="nav-item">
